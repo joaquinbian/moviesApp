8be57fb40d400ac2d82540ae36464a8531e6c03e
joaquinbian <joacobian21@gmail.com>
2021-09-28 00:32:58 -0300

    adding a slashScreen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import NavigationStack from './src/navigation/NavigationStack';
-import FadeScreen from './src/screens/FadeScreen';
 import {GradientProvider} from './src/context/GradientContext';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <NavigationContainer>
       <GradientProvider>
--- src/navigation/NavigationStack.tsx
+++ src/navigation/NavigationStack.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import SplashScreen from 'react-native-splash-screen';
 import {View, Text} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import MovieDetailScreen from '../screens/MovieDetailScreen';
 import {Movie} from '../interfaces/movieDBInterface';
+import useEffect from 'react';
 
 export type RootStackParams = {
   Home: undefined;
